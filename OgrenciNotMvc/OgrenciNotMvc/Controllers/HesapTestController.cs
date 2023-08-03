5a2b46dfb822b9cfe91dac2b6a5d5556828a929c
Meryem Talay <meyremtlly@gmail.com>
2023-08-03 16:45:42 +0300

    Öğrenci Not Sistemi

---
+++ OgrenciNotMvc/OgrenciNotMvc/Controllers/HesapTestController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OgrenciNotMvc.Models.EntityFramework;
+
+namespace OgrenciNotMvc.Controllers
+{
+    public class HesapTestController : Controller
+    {
+        // GET: HesapTest
+        public ActionResult Index(double sayi1 = 0, double sayi2 = 0)
+        {
+            // Bu sayılar üzerinde işlem yapmadan önce verilerin boş olup olmadığını kontrol edelim.
+            if (ModelState.IsValid)
+            {
+                double toplam = sayi1 + sayi2;
+                double çıkarma = sayi1 - sayi2;
+                double çarpma = sayi1 * sayi2;
+                double bölme = sayi1 / sayi2;
+
+                // Controller tarafındaki değerleri view tarafına taşır
+                ViewBag.tpl = toplam;
+                ViewBag.ckr = çıkarma;
+                ViewBag.crp = çarpma;
+                ViewBag.bol = bölme;
+                ViewBag.sayi1 = sayi1;
+                ViewBag.sayi2 = sayi2;
+            }
+
+            return View();
+        }
+
+        // POST: HesapTest
+        [HttpPost]
+        public ActionResult Index(double sayi1, double sayi2, string islem)
+        {
+            double sonuc = 0;
+            switch (islem)
+            {
+                case "topla":
+                    sonuc = sayi1 + sayi2;
+                    break;
+                case "cikar":
+                    sonuc = sayi1 - sayi2;
+                    break;
+                case "carp":
+                    sonuc = sayi1 * sayi2;
+                    break;
+                case "bol":
+                    sonuc = sayi1 / sayi2;
+                    break;
+            }
+
+            ViewBag.islemSonucu = sonuc;
+            ViewBag.sayi1 = sayi1;
+            ViewBag.sayi2 = sayi2;
+
+            return View();
+        }
+
+       
+    }
+}
+
---
+++ OgrenciNotMvc/OgrenciNotMvc/Controllers/OgrenciController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OgrenciNotMvc.Models.EntityFramework;
+
+namespace OgrenciNotMvc.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        // GET: Ogrenci
+        DbMvcOkulEntities db = new DbMvcOkulEntities();
+        public ActionResult Index()
+        {
+            var ogrenciler = db.TBLOGRENCILER.ToList();
+            return View(ogrenciler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniOgrenci()
+        {
+            //LİNQ Sorgusu
+            List<SelectListItem> degerler=(from i in db.TBLKULUPLER.ToList() 
+                                           select new SelectListItem
+                                           {
+                                               Text=i.KULUPAD,
+                                               Value=i.KULUPID.ToString()
+                                           }).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult YeniOgrenci(TBLOGRENCILER p3)
+        {
+            var klp = db.TBLKULUPLER.Where(m => m.KULUPID == p3.TBLKULUPLER.KULUPID).FirstOrDefault();
+            p3.TBLKULUPLER = klp;
+            db.TBLOGRENCILER.Add(p3);
+            db.SaveChanges(); //değişiklikleri kaydet
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var ogrenci = db.TBLOGRENCILER.Find(id);
+            db.TBLOGRENCILER.Remove(ogrenci);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult OgrenciGetir(int id)
+        {
+            var ogrenci = db.TBLOGRENCILER.Find(id);
+            return View("OgrenciGetir",ogrenci);
+        }
+        public ActionResult Guncelle(TBLOGRENCILER p)
+        {
+            var ogr = db.TBLOGRENCILER.Find(p.OGRENCIID);
+            ogr.OGAD=p.OGAD;
+            ogr.OGSOYAD = p.OGSOYAD;
+            ogr.OGFOTO = p.OGFOTO;
+            ogr.OGCINSIYET = p.OGCINSIYET;
+            ogr.OGKULUP = p.OGKULUP;
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Ogrenci");
+        }
+    }
+}
